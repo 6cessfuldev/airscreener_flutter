180dd342b272bd67d6deec5dede38de510f193fa
6cessfuldev <6cessfuldev@gmail.com>
2024-01-26 11:32:04 +0900

    커밋 뭉텅이

--- lib/screen/checkin_counter_page.dart
+++ lib/screen/checkin_counter_page.dart
@@ -1,9 +1,13 @@
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:lottie/lottie.dart';
 import 'package:slide_digital_clock/slide_digital_clock.dart';
 
 import '../common/style.dart';
+import '../model/departing_flights_list.dart';
 import '../model/terminalid_enum.dart';
+import '../service/api_service.dart';
 import '../widget/flight_info_table.dart';
 
 class CheckInCounterPage extends StatefulWidget {
@@ -16,6 +20,29 @@ class CheckInCounterPage extends StatefulWidget {
 class _CheckInCounterPageState extends State<CheckInCounterPage> {
   TerminalidEnum terminalid = TerminalidEnum.p01;
   String counterCode = 'A';
+  bool _isLoading = true;
+
+  final ApiService _apiService = ApiService();
+  List<DepartingFlightsInfo> _dataList = [];
+
+  @override
+  void initState() {
+    dataFetchAndFilter();
+    super.initState();
+
+    _apiService.getDepartingFlightsList({
+      'pageNo': 1,
+      'numOfRows': 4000,
+      'type': 'json',
+
+      // 'from_time': DateFormat('HHmm').format(DateTime.now()).toString(),
+      'searchday': DateFormat('yyyyMMdd').format(DateTime.now()).toString()
+    }).then((value) => value.items!.map((e) {
+          if (e.chkinrange!.contains('E')) {
+            print('e');
+          }
+        }));
+  }
 
   void setCounterCode(String counterCode) {
     setState(() {
@@ -29,6 +56,132 @@ class _CheckInCounterPageState extends State<CheckInCounterPage> {
     });
   }
 
+  void setLoadingStatus(bool isLoading) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        setState(() {
+          debugPrint("setState");
+          _isLoading = isLoading;
+        });
+      }
+    });
+    debugPrint('isLoading $_isLoading');
+  }
+
+  Future<void> dataFetchAndFilter() async {
+    setLoadingStatus(true);
+    await dataFetch();
+    setState(() {
+      _dataList = filterData(_dataList);
+    });
+    setLoadingStatus(false);
+  }
+
+  Future<void> dataFetch() async {
+    Map<String, dynamic> todayRequest = {
+      'pageNo': 1,
+      'numOfRows': 4000,
+      'type': 'json',
+      'from_time': DateFormat('HHmm').format(DateTime.now()).toString(),
+      'searchday': DateFormat('yyyyMMdd').format(DateTime.now()).toString()
+    };
+
+    Map<String, dynamic> tommorowRequest = {
+      'pageNo': 1,
+      'numOfRows': 2000,
+      'type': 'json',
+      'to_time': DateFormat('HHmm')
+          .format(DateTime.now().add(const Duration(hours: 24)))
+          .toString(),
+      'searchday': DateFormat('yyyyMMdd')
+          .format(DateTime.now().add(const Duration(days: 1)))
+          .toString()
+    };
+
+    List<Future> tasks = [];
+    tasks.add(
+        _apiService.getDepartingFlightsList(todayRequest, defaultData: {}));
+    tasks.add(
+        _apiService.getDepartingFlightsList(tommorowRequest, defaultData: {}));
+
+    List responseList = await Future.wait(tasks);
+
+    debugPrint('status ');
+
+    List<DepartingFlightsInfo> resultList = [];
+    if (responseList[0].status == 200) {
+      resultList.addAll(responseList[0].items);
+    }
+    if (responseList[1].status == 200) {
+      resultList.addAll(responseList[1].items);
+    }
+
+    _dataList = resultList;
+  }
+
+  List<DepartingFlightsInfo> filterData(List<DepartingFlightsInfo> resultList) {
+    return resultList.where((item) {
+      if (item.terminalid != null &&
+          !item.terminalid!
+              .contains(terminalid.toString().split('.').last.toUpperCase())) {
+        return false;
+      }
+
+      /// counterCode 선택했을 경우 filtering 로직
+      /// '-'가 포함된 경우 앞의 알파벳00 뒤의 알파벳00 사이에 올 수 있는 모든 알파벳과 코드를 계산한다
+      if (item.chkinrange != null &&
+          item.chkinrange!.contains('-') &&
+          item.chkinrange!.length >= 7) {
+        int firstChar = item.chkinrange![0].codeUnits[0];
+        int firstCode = int.parse(item.chkinrange!.substring(1, 3));
+        int lastChar = item.chkinrange![4].codeUnits[0];
+        int lastCode = int.parse(item.chkinrange!.substring(5, 7));
+
+        // debugPrint("checkin : ${item.chkinrange}");
+        // debugPrint(
+        //     "firstChar : $firstChar, firstCode : $firstCode, lastChar : $lastChar, lastCode : $lastCode");
+        // debugPrint(widget.counterCode![0].codeUnits[0].toString());
+        int selectChkChar = counterCode[0].codeUnits[0];
+        int? selectChkNum =
+            counterCode.length > 1 ? int.parse(counterCode[1]) : null;
+
+        // debugPrint(
+        //     "selectChkChar : $selectChkChar, selectChkNum : $selectChkNum");
+
+        /// 알파벳 범위 밖은 제외
+        if (selectChkChar < firstChar || selectChkChar > lastChar) {
+          return false;
+        }
+
+        /// 알파벳 범위 안에 있고 선택한 체크인 구분이 숫자가 없는 구분값일 경우는 (ex A, G, N)
+        /// 알파벳 범위 안에만 들면 되지만 B1, C2와 같이 번호가 있는 경우
+        /// 1은 1~18, 2는 19~... 코드에 해당하는 번호다.
+        /// 예를 들어 B1를 선택했을 경우 B19-C18 체크인 카운터 데이터는 필터링해야 한다.
+        /// 반대로 C2를 선택했을 경우에도 C19부터이므로 해당 B19-C18 데이터는 필터링해야 한다.
+        if (selectChkNum != null) {
+          if (selectChkChar == firstChar &&
+              selectChkNum == 1 &&
+              firstCode > 18) {
+            return false;
+          }
+          if (selectChkChar == lastChar &&
+              selectChkNum == 2 &&
+              lastCode <= 18) {
+            return false;
+          }
+        }
+      }
+      // 알파벳만 있는 경우 A B C 와 같이 모두 나열하므로 contains로 체크
+      else {
+        if (item.chkinrange != null &&
+            !item.chkinrange!.contains(counterCode[0])) {
+          return false;
+        }
+      }
+      return true;
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -36,9 +189,12 @@ class _CheckInCounterPageState extends State<CheckInCounterPage> {
         const SizedBox(
           height: 20,
         ),
-        clockContainer(),
+        // clockContainer(),
         selectBox(),
-        FlightInfoTable(terminalid: terminalid, counterCode: counterCode),
+        FlightInfoTable(
+          dataList: _dataList,
+          isLoading: _isLoading,
+        ),
         const SizedBox(
           height: 80,
         )
@@ -47,8 +203,8 @@ class _CheckInCounterPageState extends State<CheckInCounterPage> {
   }
 
   Widget clockContainer() {
-    double width = 300;
-    double height = 100;
+    double width = 200;
+    double height = 70;
     return SizedBox(
       width: width,
       height: height,
@@ -81,17 +237,17 @@ class _CheckInCounterPageState extends State<CheckInCounterPage> {
                 amPmDigitTextStyle: const TextStyle(fontFamily: 'Montserrat'),
                 hourMinuteDigitTextStyle: Theme.of(context)
                     .textTheme
-                    .displayLarge!
+                    .headlineMedium!
                     .copyWith(color: Colors.white70),
                 secondDigitTextStyle: Theme.of(context)
                     .textTheme
-                    .displayLarge!
+                    .headlineMedium!
                     .copyWith(color: Colors.white),
                 colon: Text(
                   ":",
                   style: Theme.of(context)
                       .textTheme
-                      .displayLarge!
+                      .headlineMedium!
                       .copyWith(color: Colors.white),
                 ),
               ),
@@ -116,9 +272,14 @@ class _CheckInCounterPageState extends State<CheckInCounterPage> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           TerminalSelectBox(
-              selectedValue: terminalid, setSelectedValue: setTerminalid),
+              reload: dataFetchAndFilter,
+              selectedValue: terminalid,
+              setSelectedValue: setTerminalid),
           CheckInCounterSelectBox(
-              selectedValue: counterCode, setSelectedValue: setCounterCode),
+              reload: dataFetchAndFilter,
+              selectedValue: counterCode,
+              setSelectedValue: setCounterCode),
+          ReloadButton(reload: dataFetchAndFilter, isLoading: _isLoading),
         ],
       ),
     );
@@ -127,8 +288,12 @@ class _CheckInCounterPageState extends State<CheckInCounterPage> {
 
 class CheckInCounterSelectBox extends StatefulWidget {
   const CheckInCounterSelectBox(
-      {super.key, required this.selectedValue, required this.setSelectedValue});
+      {super.key,
+      required this.reload,
+      required this.selectedValue,
+      required this.setSelectedValue});
 
+  final Function reload;
   final String? selectedValue;
   final Function setSelectedValue;
 
@@ -217,6 +382,7 @@ class _CheckInCounterSelectBoxState extends State<CheckInCounterSelectBox> {
             value: widget.selectedValue,
             onChanged: (value) {
               widget.setSelectedValue(value);
+              widget.reload();
             },
             onMenuStateChange: (isOpen) {
               setState(() {
@@ -282,8 +448,12 @@ class _CheckInCounterSelectBoxState extends State<CheckInCounterSelectBox> {
 
 class TerminalSelectBox extends StatefulWidget {
   const TerminalSelectBox(
-      {super.key, required this.selectedValue, required this.setSelectedValue});
+      {super.key,
+      required this.reload,
+      required this.selectedValue,
+      required this.setSelectedValue});
 
+  final Function reload;
   final TerminalidEnum? selectedValue;
   final Function setSelectedValue;
 
@@ -292,7 +462,7 @@ class TerminalSelectBox extends StatefulWidget {
 }
 
 class _TerminalSelectBoxState extends State<TerminalSelectBox> {
-  final List<TerminalidEnum> items = TerminalidEnum.values;
+  List<TerminalidEnum> items = TerminalidEnum.values;
 
   bool isClicked = false;
   bool isSelected = false;
@@ -342,6 +512,7 @@ class _TerminalSelectBoxState extends State<TerminalSelectBox> {
             onChanged: (value) {
               if (value != null) {
                 widget.setSelectedValue(TerminalidEnum.convertStrToEnum(value));
+                widget.reload();
               }
             },
             onMenuStateChange: (isOpen) {
@@ -399,6 +570,98 @@ class _TerminalSelectBoxState extends State<TerminalSelectBox> {
               height: 40,
               padding: EdgeInsets.only(left: 14, right: 14),
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ReloadButton extends StatefulWidget {
+  const ReloadButton(
+      {required this.reload, required this.isLoading, super.key});
+
+  final Function reload;
+  final bool isLoading;
+
+  @override
+  State<ReloadButton> createState() => _ReloadBoxState();
+}
+
+class _ReloadBoxState extends State<ReloadButton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void didUpdateWidget(covariant oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.isLoading && !widget.isLoading) {
+      controller.stop();
+    } else if (!oldWidget.isLoading && widget.isLoading) {
+      controller.repeat();
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(outsideShadowDistance),
+      child: GestureDetector(
+        onTap: () {
+          debugPrint("isLoading ${widget.isLoading}");
+          if (widget.isLoading) return;
+          widget.reload();
+          controller.repeat();
+        },
+        child: Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(14.0),
+            boxShadow: widget.isLoading
+                ? null
+                : [
+                    BoxShadow(
+                        offset: const Offset(
+                            outsideShadowDistance, outsideShadowDistance),
+                        color: Colors.blue.shade300,
+                        blurRadius: outsideShadowDistance,
+                        spreadRadius: 2),
+                    const BoxShadow(
+                        offset: Offset(
+                            -outsideShadowDistance, -outsideShadowDistance),
+                        color: Colors.white60,
+                        blurRadius: outsideShadowDistance,
+                        spreadRadius: 2)
+                  ],
+          ),
+          child: Lottie.asset(
+            'assets/animations/reload_animation.json',
+            controller: controller,
+            onLoaded: (composition) {
+              controller.duration = composition.duration;
+              controller.forward();
+            },
+            frameBuilder: (context, child, composition) {
+              return ColorFiltered(
+                colorFilter:
+                    const ColorFilter.mode(Colors.blue, BlendMode.srcATop),
+                child: child,
+              );
+            },
           ),
         ),
       ),
--- lib/widget/flight_info_table.dart
+++ lib/widget/flight_info_table.dart
@@ -1,253 +1,131 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import '../common/style.dart';
 import '../model/departing_flights_list.dart';
-import '../model/terminalid_enum.dart';
-import '../service/api_service.dart';
 
 class FlightInfoTable extends StatefulWidget {
-  const FlightInfoTable({this.terminalid, this.counterCode, super.key});
+  const FlightInfoTable(
+      {super.key, required this.dataList, required this.isLoading});
 
-  final TerminalidEnum? terminalid;
-  final String? counterCode;
+  final List<DepartingFlightsInfo> dataList;
+  final bool isLoading;
 
   @override
   State<FlightInfoTable> createState() => _FlightInfoTableState();
 }
 
 class _FlightInfoTableState extends State<FlightInfoTable> {
-  late final ApiService _apiService = ApiService();
-  bool isClicked = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _dataFetch();
-  }
-
-  Future<List<DepartingFlightsInfo>> _dataFetch() async {
-    Map<String, dynamic> todayRequest = {
-      'pageNo': 1,
-      'numOfRows': 2000,
-      'type': 'json',
-      'from_time': DateFormat('HHmm').format(DateTime.now()).toString(),
-      'searchday': DateFormat('yyyyMMdd').format(DateTime.now()).toString()
-    };
-
-    Map<String, dynamic> tommorowRequest = {
-      'pageNo': 1,
-      'numOfRows': 2000,
-      'type': 'json',
-      'to_time': DateFormat('HHmm')
-          .format(DateTime.now().add(const Duration(hours: 12)))
-          .toString(),
-      'searchday': DateFormat('yyyyMMdd')
-          .format(DateTime.now().add(const Duration(days: 1)))
-          .toString()
-    };
-
-    List<Future> tasks = [];
-    tasks.add(
-        _apiService.getDepartingFlightsList(todayRequest, defaultData: {}));
-    tasks.add(
-        _apiService.getDepartingFlightsList(tommorowRequest, defaultData: {}));
-
-    List responseList = await Future.wait(tasks);
-
-    // debugPrint(responseList[0].status.toString());
-
-    List<DepartingFlightsInfo> resultList = [];
-    if (responseList[0].status == 200) {
-      resultList.addAll(responseList[0].items);
-    }
-    if (responseList[1].status == 200) {
-      resultList.addAll(responseList[1].items);
-    }
-
-    // debugPrint(resultList.length.toString());
-
-    return resultList.where((item) {
-      if (widget.terminalid != null &&
-          item.terminalid != null &&
-          !item.terminalid!.contains(
-              widget.terminalid!.toString().split('.').last.toUpperCase())) {
-        return false;
-      }
-
-      /// counterCode 선택했을 경우 filtering 로직
-      if (widget.counterCode != null) {
-        /// '-'가 포함된 경우 앞의 알파벳00 뒤의 알파벳00 사이에 올 수 있는 모든 알파벳과 코드를 계산한다
-        if (item.chkinrange != null &&
-            item.chkinrange!.contains('-') &&
-            item.chkinrange!.length >= 7) {
-          int firstChar = item.chkinrange![0].codeUnits[0];
-          int firstCode = int.parse(item.chkinrange!.substring(1, 3));
-          int lastChar = item.chkinrange![4].codeUnits[0];
-          int lastCode = int.parse(item.chkinrange!.substring(5, 7));
-
-          debugPrint("checkin : ${item.chkinrange}");
-          debugPrint(
-              "firstChar : $firstChar, firstCode : $firstCode, lastChar : $lastChar, lastCode : $lastCode");
-          debugPrint(widget.counterCode![0].codeUnits[0].toString());
-          int selectChkChar = widget.counterCode![0].codeUnits[0];
-          int? selectChkNum = widget.counterCode!.length > 1
-              ? int.parse(widget.counterCode![1])
-              : null;
-
-          debugPrint(
-              "selectChkChar : $selectChkChar, selectChkNum : $selectChkNum");
-
-          /// 알파벳 범위 밖은 제외
-          if (selectChkChar < firstChar || selectChkChar > lastChar) {
-            return false;
-          }
-
-          /// 알파벳 범위 안에 있고 선택한 체크인 구분이 숫자가 없는 구분값일 경우는 (ex A, G, N)
-          /// 알파벳 범위 안에만 들면 되지만 B1, C2와 같이 번호가 있는 경우
-          /// 1은 1~18, 2는 19~... 코드에 해당하는 번호다.
-          /// 예를 들어 B1를 선택했을 경우 B19-C18 체크인 카운터 데이터는 필터링해야 한다.
-          /// 반대로 C2를 선택했을 경우에도 C19부터이므로 해당 B19-C18 데이터는 필터링해야 한다.
-          if (selectChkNum != null) {
-            if (selectChkChar == firstChar &&
-                selectChkNum == 1 &&
-                firstCode > 18) {
-              return false;
-            }
-            if (selectChkChar == lastChar &&
-                selectChkNum == 2 &&
-                lastCode <= 18) {
-              return false;
-            }
-          }
-        }
-        // 알파벳만 있는 경우 A B C 와 같이 모두 나열하므로 contains로 체크
-        else {
-          if (item.chkinrange != null &&
-              !item.chkinrange!.contains(widget.counterCode!)) {
-            return false;
-          }
-        }
-      }
-      return true;
-    }).toList();
-  }
-
   String convertDateFormat(String? rawDate) {
     if (rawDate == null || rawDate == '') return '';
-    return '${rawDate.substring(4, 6)}/${rawDate.substring(6, 8)} ${rawDate.substring(8, 10)}:${rawDate.substring(10, 12)}';
+    return '${rawDate.substring(8, 10)}:${rawDate.substring(10, 12)}';
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _dataFetch(),
-        builder: (context, snapshot) {
-          return GestureDetector(
-            onTapDown: (_) {},
-            child: Container(
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                color: backgroundColor,
-                borderRadius: BorderRadius.circular(14),
-                boxShadow: [
-                  BoxShadow(
-                      offset: const Offset(
-                          outsideShadowDistance, outsideShadowDistance),
-                      color: Colors.blue.shade300,
-                      blurRadius: outsideShadowDistance,
-                      spreadRadius: 5),
-                  const BoxShadow(
-                      offset: Offset(
-                          -outsideShadowDistance, -outsideShadowDistance),
-                      color: Colors.white60,
-                      blurRadius: outsideShadowDistance,
-                      spreadRadius: 5)
-                ],
-              ),
-              child: ClipRRect(
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    child: Table(
-                      border: TableBorder.all(
-                        borderRadius: BorderRadius.circular(8.0),
-                        color: Colors.transparent,
-                      ),
-                      columnWidths: const <int, TableColumnWidth>{
-                        0: FlexColumnWidth(1.5),
-                        1: FlexColumnWidth(2),
-                        2: FlexColumnWidth(3),
-                        3: FlexColumnWidth(3),
-                      },
-                      defaultVerticalAlignment:
-                          TableCellVerticalAlignment.middle,
-                      children: [
-                        const TableRow(children: [
-                          Center(
-                              child: Text(
-                            '터미널',
-                            style: TextStyle(color: Colors.blueAccent),
-                            textAlign: TextAlign.center,
-                          )),
-                          Center(
-                              child: Text(
-                            '카운터',
-                            style: TextStyle(color: Colors.blueAccent),
-                            textAlign: TextAlign.center,
-                          )),
-                          Center(
-                              child: Text(
-                            '항공사',
-                            style: TextStyle(color: Colors.blueAccent),
-                            textAlign: TextAlign.center,
-                          )),
-                          Center(
-                              child: Text(
-                            '출발 시간',
-                            style: TextStyle(color: Colors.blueAccent),
-                            textAlign: TextAlign.center,
-                          )),
-                        ]),
-                        if (snapshot.connectionState !=
-                                ConnectionState.waiting &&
-                            !snapshot.hasError)
-                          ...?snapshot.data?.map((info) => TableRow(children: [
-                                Center(
-                                    child: Text(
-                                  info.terminalid ?? '',
-                                  textAlign: TextAlign.center,
-                                )),
-                                SizedBox(
-                                    height: 40,
-                                    child: Center(
-                                        child: Text(
-                                      info.chkinrange ?? '',
-                                      textAlign: TextAlign.center,
-                                      style: const TextStyle(
-                                          color: Colors.blueGrey),
-                                    ))),
-                                Center(
-                                    child: Text(
-                                  info.airline ?? '',
-                                  textAlign: TextAlign.center,
-                                )),
-                                // Center(child: Text(info.scheduleDateTime ?? '')),
-                                Center(
-                                    child: Text(
-                                  convertDateFormat(info.estimatedDateTime),
-                                  textAlign: TextAlign.center,
-                                )),
-                              ]))
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(14),
+        boxShadow: [
+          BoxShadow(
+              offset:
+                  const Offset(outsideShadowDistance, outsideShadowDistance),
+              color: Colors.blue.shade300,
+              blurRadius: outsideShadowDistance,
+              spreadRadius: 5),
+          const BoxShadow(
+              offset: Offset(-outsideShadowDistance, -outsideShadowDistance),
+              color: Colors.white60,
+              blurRadius: outsideShadowDistance,
+              spreadRadius: 5)
+        ],
+      ),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.95,
+        height: 550,
+        child: SingleChildScrollView(
+          child: Table(
+            border: TableBorder.all(
+              borderRadius: BorderRadius.circular(8.0),
+              color: Colors.transparent,
             ),
-          );
-        });
+            columnWidths: const <int, TableColumnWidth>{
+              0: FlexColumnWidth(1.5),
+              1: FlexColumnWidth(2),
+              2: FlexColumnWidth(3),
+              3: FlexColumnWidth(1),
+            },
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            children: [
+              const TableRow(children: [
+                Center(
+                    child: Text(
+                  '출발 시간',
+                  style: TextStyle(color: Colors.blueAccent),
+                  textAlign: TextAlign.center,
+                )),
+                Center(
+                    child: Text(
+                  '편명',
+                  style: TextStyle(color: Colors.blueAccent),
+                  textAlign: TextAlign.center,
+                )),
+                Center(
+                    child: Text(
+                  '도착지・경유지',
+                  style: TextStyle(color: Colors.blueAccent),
+                  textAlign: TextAlign.center,
+                )),
+                Center(
+                    child: Text(
+                  '게이트',
+                  style: TextStyle(color: Colors.blueAccent),
+                  textAlign: TextAlign.center,
+                )),
+              ]),
+              ...widget.dataList.map((info) => TableRow(children: [
+                    Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          convertDateFormat(info.scheduleDateTime),
+                          textAlign: TextAlign.center,
+                        ),
+                        if (info.estimatedDateTime != info.scheduleDateTime)
+                          Text(
+                            '-> ${convertDateFormat(info.estimatedDateTime)}',
+                            style: const TextStyle(color: Colors.red),
+                            textAlign: TextAlign.center,
+                          ),
+                      ],
+                    )),
+                    
+                    SizedBox(
+                        height: 45,
+                        child: Center(
+                            child: Text(
+                          info.flightId ?? '',
+                          textAlign: TextAlign.center,
+                        ))),
+                    Center(
+                        child: Text(
+                      info.airport ?? '',
+                      textAlign: TextAlign.center,
+                    )),
+                    Center(
+                        child: Text(
+                      info.gatenumber ?? '',
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(color: Colors.blueGrey),
+                    )),
+                    // Center(child: Text(info.scheduleDateTime ?? '')),
+                    
+                  ]))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
