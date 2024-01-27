6c6035731856a00c109f2b22c726d70cd888b712
6cessfuldev <6cessfuldev@gmail.com>
2024-01-27 12:51:16 +0900

    Feat(TerminalidEnum) : delete '탑승동'

--- lib/model/terminalid_enum.dart
+++ lib/model/terminalid_enum.dart
@@ -1,15 +1,12 @@
 enum TerminalidEnum {
   p01,
-  p02,
-  p03;
+  p02;
 
   String get convertEnumToStr {
     switch (this) {
       case TerminalidEnum.p01:
         return '제1터미널';
       case TerminalidEnum.p02:
-        return '탑승동';
-      case TerminalidEnum.p03:
         return '제2터미널';
       default:
         return '';
@@ -20,10 +17,8 @@ enum TerminalidEnum {
     switch (str) {
       case '제1터미널':
         return TerminalidEnum.p01;
-      case '탑승동':
-        return TerminalidEnum.p02;
       case '제2터미널':
-        return TerminalidEnum.p03;
+        return TerminalidEnum.p02;
       default:
         return null;
     }
